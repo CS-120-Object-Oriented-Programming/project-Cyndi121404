b453ebcfbb3e6cc798ef1f0c718bb48e64a2770f
cyndisanchez <cyndisanchez@localhost>
2024-02-21 15:00:06 -0500

    Added myself as author

--- src/Game.java
+++ src/Game.java
@@ -9,6 +9,7 @@
  * commands that the parser returns.
  *
  * @author Maria Jump
+ * @author Cyndi Sanchez
  * @version 2015.02.01
  *
  * Used with permission from Dr. Maria Jump at Northeastern University
